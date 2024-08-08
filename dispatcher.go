002438e80ee165bd6aee0896b809b9bf8f2fbec4
David Mullen <dangermouse12885@gmail.com>
2024-08-08 18:09:45 -0400

    changing dispatch of parse-feed and download-upload to batches for better reliability

--- dispatcher.go
+++ dispatcher.go
@@ -131,164 +131,182 @@ func main() {
 
 	count := len(feeds)
 	fmt.Printf(os.Getenv("env")+" Sending %d requests to Parse Feeds...\n", count)
-	wgPF := sync.WaitGroup{}
 
 	// URL to the Function
 	pfUrl := os.Getenv("parse_feed_url")
 
-	// Create channel to store Parse Feed responses
-	parsedDataCh := make(chan ParsedData, int(count/4)+count%4)
+	// Run Dispatcher for every feed in batches of 200
 
-	i := 0
-	for i < count {
-		var payloadFeeds []Feed
-		if i+3 >= count {
-			payloadFeeds = append(payloadFeeds, feeds[i])
-			i++
-		} else {
-			payloadFeeds = append(payloadFeeds, feeds[i])
-			payloadFeeds = append(payloadFeeds, feeds[i+1])
-			payloadFeeds = append(payloadFeeds, feeds[i+2])
-			payloadFeeds = append(payloadFeeds, feeds[i+3])
-			i = i + 4
-		}
-
-		feedPayload := FeedPayload{
-			FeedList: payloadFeeds,
-		}
-		payloadJson, _ := json.Marshal(feedPayload)
-		wgPF.Add(1)
-		go func(i int, payloadJson []byte) {
-			defer wgPF.Done()
-			sleep := 1
-			for j := 0; j < 10; j++ {
-				res, err := http.Post(pfUrl, "application/json", bytes.NewBuffer(payloadJson))
-
-				if err == nil && res.StatusCode/100 == 2 {
-					body := []byte{}
-					body, _ = ioutil.ReadAll(res.Body)
-					parsedData := ParsedData{
-						Body:         body,
-						ParsedStatus: 0,
-					}
-					parsedDataCh <- parsedData
-					break
-				}
-
-				// Something went wrong, pause and try again
-				body := []byte{}
-				if res != nil {
-					body, _ = ioutil.ReadAll(res.Body)
-				}
-				if j >= 9 {
-					// Done retries, store error and exit
-					fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s); payload: (%s)\n",
-						i/4, err, res.StatusCode, string(body), string(payloadJson))
-					parsedData := ParsedData{
-						Body:         body,
-						ParsedStatus: 1,
-					}
-					parsedDataCh <- parsedData
-					break
-				}
-				fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s)\n",
-					i/4, err, res.StatusCode, string(body))
-				time.Sleep(time.Second * time.Duration(sleep))
-				sleep *= 2
-			}
-		}(i, payloadJson)
-	}
-
-	// Wait for all threads to finish before we exit
-	wgPF.Wait()
-	close(parsedDataCh)
-	fmt.Printf(os.Getenv("env") + " Done Dispatching Feeds\n")
-
-	// Gather Data From Channel
+	startIndex := 0
+	batchCount := 1
 	var allParsedData [][]byte
 	numErrors := 0
-	for chValue := range parsedDataCh {
-		if chValue.ParsedStatus == 0 {
-			allParsedData = append(allParsedData, chValue.Body)
-		} else {
-			numErrors++
+	for startIndex < count {
+		wgPF := sync.WaitGroup{}
+
+		// Create channel to store Parse Feed responses
+		parsedDataCh := make(chan ParsedData, min(200, count-startIndex))
+
+		i := startIndex
+		for i < startIndex+200 && i < count {
+			var payloadFeeds []Feed
+			payloadFeeds = append(payloadFeeds, feeds[i])
+			i++
+
+			feedPayload := FeedPayload{
+				FeedList: payloadFeeds,
+			}
+			payloadJson, _ := json.Marshal(feedPayload)
+			wgPF.Add(1)
+			go func(i int, payloadJson []byte) {
+				defer wgPF.Done()
+				sleep := 1
+				for j := 0; j < 10; j++ {
+					res, err := http.Post(pfUrl, "application/json", bytes.NewBuffer(payloadJson))
+
+					if err == nil && res.StatusCode/100 == 2 {
+						body := []byte{}
+						body, _ = ioutil.ReadAll(res.Body)
+						parsedData := ParsedData{
+							Body:         body,
+							ParsedStatus: 0,
+						}
+						parsedDataCh <- parsedData
+						break
+					}
+
+					// Something went wrong, pause and try again
+					body := []byte{}
+					if res != nil {
+						body, _ = ioutil.ReadAll(res.Body)
+					}
+					if j >= 9 {
+						// Done retries, store error and exit
+						fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s); payload: (%s)\n",
+							i/4, err, res.StatusCode, string(body), string(payloadJson))
+						parsedData := ParsedData{
+							Body:         body,
+							ParsedStatus: 1,
+						}
+						parsedDataCh <- parsedData
+						break
+					}
+					fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s)\n",
+						i/4, err, res.StatusCode, string(body))
+					time.Sleep(time.Second * time.Duration(sleep))
+					sleep *= 2
+				}
+			}(i, payloadJson)
 		}
+
+		// Wait for all threads to finish before we exit
+		wgPF.Wait()
+		close(parsedDataCh)
+
+		// Gather Data From Channel
+		for chValue := range parsedDataCh {
+			if chValue.ParsedStatus == 0 {
+				allParsedData = append(allParsedData, chValue.Body)
+			} else {
+				numErrors++
+			}
+		}
+
+		fmt.Printf(os.Getenv("env")+" Parse Feed Batch #: %d Completed with %d Errors so far\n", batchCount, numErrors)
+
+		startIndex = startIndex + 200
+		batchCount++
 	}
+	fmt.Printf(os.Getenv("env") + " Done Dispatching Feeds\n")
 
 	fmt.Printf(os.Getenv("env")+" Dispatched %d feeds with %d Errors\n", count, numErrors)
 
 	count = len(allParsedData)
 	fmt.Printf(os.Getenv("env")+" Sending %d requests to Download Upload Feed...\n", count)
-	wgDUF := sync.WaitGroup{}
 
 	// URL to the Function
 	dufUrl := os.Getenv("download_upload_url")
 
-	// Create channel to store Download Upload responses
-	leadsDataCh := make(chan LeadsData, count)
+	// Run Download-Upload for every feed in batches of 200
 
-	for i = 0; i < count; i++ {
-		wgDUF.Add(1)
-		go func(i int, payloadJson []byte) {
-			defer wgDUF.Done()
-			sleep := 1
-			for j := 0; j < 10; j++ {
-				res, err := http.Post(dufUrl, "application/json", bytes.NewBuffer(payloadJson))
-
-				if err == nil && res.StatusCode/100 == 2 {
-					var dufRes DufRes
-					err := json.NewDecoder(res.Body).Decode(&dufRes)
-					if err != nil {
-						fmt.Println("JSON decode for DOWNLOAD UPLOAD FEED RESPONSE error!")
-						panic(err)
-					}
-					leadsData := LeadsData{
-						Leads:        dufRes.Leads,
-						DownUpStatus: 0,
-					}
-					leadsDataCh <- leadsData
-					break
-				}
-
-				// Something went wrong, pause and try again
-				body := []byte{}
-				if res != nil {
-					body, _ = ioutil.ReadAll(res.Body)
-				}
-				fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s); payload size: (%d)\n",
-					i, err, res.StatusCode, string(body), len(payloadJson))
-				if j >= 9 {
-					// Done retries, store error in Channel and exit
-					leadsData := LeadsData{
-						Leads:        nil,
-						DownUpStatus: 1,
-					}
-					leadsDataCh <- leadsData
-					break
-				}
-				time.Sleep(time.Second * time.Duration(sleep))
-				sleep *= 2
-			}
-		}(i, allParsedData[i])
-	}
-
-	// Wait for all threads to finish before we exit
-	wgDUF.Wait()
-	close(leadsDataCh)
-	fmt.Printf(os.Getenv("env") + " Done Downloading/Uploading Feeds\n")
-
-	// Gather Data From Channel
-	var allLeads []string
+	startIndex = 0
+	batchCount = 1
 	numErrors = 0
-	for chValue := range leadsDataCh {
-		if chValue.DownUpStatus == 0 {
-			for _, lead := range chValue.Leads {
-				allLeads = append(allLeads, lead)
-			}
-		} else {
-			numErrors++
+	var allLeads []string
+	for startIndex < count {
+		wgDUF := sync.WaitGroup{}
+
+		// Create channel to store Download Upload responses
+		leadsDataCh := make(chan LeadsData, min(200, count-startIndex))
+
+		i := startIndex
+		for i < startIndex+200 && i < count {
+			wgDUF.Add(1)
+			go func(i int, payloadJson []byte) {
+				defer wgDUF.Done()
+				sleep := 1
+				for j := 0; j < 10; j++ {
+					res, err := http.Post(dufUrl, "application/json", bytes.NewBuffer(payloadJson))
+
+					if err == nil && res.StatusCode/100 == 2 {
+						var dufRes DufRes
+						err := json.NewDecoder(res.Body).Decode(&dufRes)
+						if err != nil {
+							fmt.Println("JSON decode for DOWNLOAD UPLOAD FEED RESPONSE error!")
+							panic(err)
+						}
+						leadsData := LeadsData{
+							Leads:        dufRes.Leads,
+							DownUpStatus: 0,
+						}
+						leadsDataCh <- leadsData
+						break
+					}
+
+					// Something went wrong, pause and try again
+					body := []byte{}
+					if res != nil {
+						body, _ = ioutil.ReadAll(res.Body)
+					}
+					fmt.Fprintf(os.Stderr, os.Getenv("env")+" Thread #: (%d); err: (%s); status: (%d); body: (%s); payload size: (%d)\n",
+						i, err, res.StatusCode, string(body), len(payloadJson))
+					if j >= 9 {
+						// Done retries, store error in Channel and exit
+						leadsData := LeadsData{
+							Leads:        nil,
+							DownUpStatus: 1,
+						}
+						leadsDataCh <- leadsData
+						break
+					}
+					time.Sleep(time.Second * time.Duration(sleep))
+					sleep *= 2
+				}
+			}(i, allParsedData[i])
 		}
+
+		// Wait for all threads to finish before we exit
+		wgDUF.Wait()
+		close(leadsDataCh)
+
+		// Gather Data From Channel
+		for chValue := range leadsDataCh {
+			if chValue.DownUpStatus == 0 {
+				for _, lead := range chValue.Leads {
+					allLeads = append(allLeads, lead)
+				}
+			} else {
+				numErrors++
+			}
+		}
+
+		fmt.Printf(os.Getenv("env")+" Download-Upload Batch #: %d Completed with %d Errors so far\n", batchCount, numErrors)
+
+		startIndex = startIndex + 200
+		batchCount++
 	}
+
+	fmt.Printf(os.Getenv("env") + " Done Downloading/Uploading Feeds\n")
 
 	fmt.Printf(os.Getenv("env")+" Dispatched %d download/upload threads with %d Errors\n", count, numErrors)
 
@@ -300,14 +318,14 @@ func main() {
 
 	// Run Lead by article for every lead in batches of 200
 
-	startIndex := 0
-	batchCount := 1
+	startIndex = 0
+	batchCount = 1
 	totalLeadsCreated := 0
 	for startIndex < count {
 		wgLBA := sync.WaitGroup{}
 		// Create channel to store Lead By Article responses
 		lbaResCh := make(chan LBAResults, min(200, count-startIndex))
-		i = startIndex
+		i := startIndex
 		for i < startIndex+200 && i < count {
 			params := url.Values{}
 			params.Add("article_id", allLeads[i])
